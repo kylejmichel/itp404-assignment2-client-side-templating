160302d083672cf59e2f8b771e8135848d888ee2
Kyle <kjmichel@usc.edu>
2019-09-08 12:49:27 -0700

    Error navigating JSON fixed

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-  const postsTemplate = Handlebars.compile(
+const postsTemplate = Handlebars.compile(
   document.getElementById("postsTemplate").innerHTML
-  )
+)
 
- $(".search").on("submit", function(e){
-   e.preventDefault();
+Handlebars.registerHelper('commas', function(subscribers) {
+  return Number(subscribers).toLocaleString('en')
+})
+
+$(".search").on("submit", async function(e){
+  e.preventDefault();
   let loader = document.createDocumentFragment();
   let loading = document.createElement('div');
   $(loading).addClass('loader');
   $(loading).html('Loading...')
   loader.append(loading)
   $('#results').html(loader)
-
-  let q = $('input[name="search"]').val();
-
-    $.ajax({
+  try{
+    let q = $('input[name="search"]').val();
+    let posts = await $.ajax({
       type: 'GET',
       url: 'https://www.reddit.com/r/' + q + '.json'
-    }).then((posts) =>  {
-
-      console.log(posts)
-      let sanitzedHtml = postsTemplate({
-        posts: posts.data.children
-      });
-
-      $('#results').html(sanitzedHtml)
-
     });
+    let sanitzedHtml = postsTemplate({
+      posts: posts.data.children
+    });
+    $('#results').html(sanitzedHtml)
+  } catch(error) {
+    $('#results').html('Oops, something went wrong!')
+  }
 });
